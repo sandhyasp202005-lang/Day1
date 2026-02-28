49413b403b408b78b8c6992a190ded7270b37b14
sandhyasp202005-lang <sandhyasp202005@gmail.com>
2026-02-28 12:13:01 +0530

    initial commit

---
+++ src/IntegerRanges.java
@@ -0,0 +1,8 @@
+public class IntegerRanges {
+    static void main(String[] args) {
+        System.out.println("Integer Max Value:"+Integer.MAX_VALUE);     //2^31-1
+        System.out.println("Integer Min Value:"+Integer.MIN_VALUE);     //-2^31
+        System.out.println("Max+1:"+Integer.MIN_VALUE+1);
+        System.out.println("Max-1:"+(Integer.MAX_VALUE-1));
+    }
+}
